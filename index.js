ce72131681f73752df26b7627b7c5841b61be235
Wesley Araújo <89321125+wesleyara@users.noreply.github.com>
2021-11-22 23:11:22 -0300

    adicionando todos índices pertinentes

--- index.js
+++ index.js
@@ -12,17 +12,53 @@ estado.toLowerCase();
 var cidadeReplace = cidade.replaceAll(" ", "-")
 
 async function geobot() {
-  const browser = await puppeteer.launch({headless: false});
+  const browser = await puppeteer.launch({headless: true});
   const page = await browser.newPage();
   await page.goto(`https://www.ibge.gov.br/cidades-e-estados/${estado}/${cidadeReplace}`);
+
+  const nomeCidade = await page.evaluate(() => {
+    return document.querySelectorAll("h1")[1].textContent
+  })
+  
+  const area = await page.evaluate(() => {
+    return document.querySelectorAll('.ind-value')[2].textContent
+  })
+  
+  const populacao = await page.evaluate(() => {
+    return document.querySelectorAll('.ind-value')[3].textContent
+  })
 
   const densidade = await page.evaluate(() => {
     return document.querySelectorAll('.ind-value')[4].textContent
   })
 
-  console.log(`A cidade tem ${densidade} de densidade demográfica.`)
+  const escolarizacao = await page.evaluate(() => {
+    return document.querySelectorAll('.ind-value')[5].textContent
+  })
+
+  const idhm = await page.evaluate(() => {
+    return document.querySelectorAll('.ind-value')[6].textContent
+  })
+
+  const mortalizadeInfantil = await page.evaluate(() => {
+    return document.querySelectorAll('.ind-value')[7].textContent
+  })
+
+  const pib = await page.evaluate(() => {
+    return document.querySelectorAll('.ind-value')[10].textContent
+  })
+
+  console.log(`A cidade de ${nomeCidade} tem ${area} de área territorial;`)
+  console.log(`População total de ${populacao};`)
+  console.log(`${densidade} de densidade demográfica;`)
+  console.log(`O indíce de escolarização de 6 a 14 anos é de ${escolarizacao};`)
+  console.log(`O IDHM é de ${idhm};`)
+  console.log(`Tem uma taxa de mortalidade infantil de ${mortalizadeInfantil};`)
+  console.log(`E um PIB per capita de ${pib}.`)
 
   await browser.close();
 };
 
 geobot();
+
+/* AVISO: Não coloque acentuação nem capslock nas pesquisas dentro do terminal */
